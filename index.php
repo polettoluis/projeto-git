ce3fcc2e8020f3cd2c0fa4bf6243e2b5d83ce8b8
polettoluis <luissfer283@gmail.com>
2026-09-15 10:55:00 -0300

    add equipe

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Projeto GIT</h1>
+    <h2>Integrantes</h2>
+    <ul>
+        <li>Nome do integrante A</li>
+        <li>Nome do integrante B</li>
+    </ul>
+
 </body>
+
 </html>
